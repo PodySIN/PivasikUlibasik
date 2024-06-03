53d5616433e12d0c7e973a865d780362b00f49ef
Pody <daniil2007xboctob@gmail.com>
2024-06-04 01:28:07 +0300

    add more pep8

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-buw9rdk*xr(eqkr3a=f)r%0tyo9u9=5celo@os@-_iu)&h+1!r"
+SECRET_KEY = (
+    "django-insecure-buw9rdk*xr(eqkr3a=f)r%0tyo9u9=5celo@os@-_iu)&h+1!r"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -30,7 +30,9 @@ class Migration(migrations.Migration):
                 ("Name", models.CharField(default="Балтика", max_length=128)),
                 (
                     "Image",
-                    models.CharField(default="images/beers/baltika_7.jpg", max_length=128),
+                    models.CharField(
+                        default="images/beers/baltika_7.jpg", max_length=128
+                    ),
                 ),
                 ("Amount", models.FloatField(default=500)),
                 ("Strength", models.FloatField(default=9.9)),
@@ -95,10 +97,15 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "Shop_id",
-                    models.IntegerField(default=1, primary_key=True, serialize=False),
+                    models.IntegerField(
+                        default=1, primary_key=True, serialize=False
+                    ),
                 ),
                 ("Job", models.BinaryField(default=1)),
-                ("Address", models.CharField(default="Бульвар", max_length=256)),
+                (
+                    "Address",
+                    models.CharField(default="Бульвар", max_length=256),
+                ),
             ],
         ),
         migrations.CreateModel(
@@ -113,10 +120,15 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
-                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "password",
+                    models.CharField(max_length=128, verbose_name="password"),
+                ),
                 (
                     "last_login",
-                    models.DateTimeField(blank=True, null=True, verbose_name="last login"),
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
                 ),
                 (
                     "is_superuser",
@@ -129,25 +141,35 @@ class Migration(migrations.Migration):
                 (
                     "username",
                     models.CharField(
-                        error_messages={"unique": "A user with that username already exists."},
+                        error_messages={
+                            "unique": "A user with that username already exists."
+                        },
                         help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
                         max_length=150,
                         unique=True,
-                        validators=[django.contrib.auth.validators.UnicodeUsernameValidator()],
+                        validators=[
+                            django.contrib.auth.validators.UnicodeUsernameValidator()
+                        ],
                         verbose_name="username",
                     ),
                 ),
                 (
                     "first_name",
-                    models.CharField(blank=True, max_length=150, verbose_name="first name"),
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="first name"
+                    ),
                 ),
                 (
                     "last_name",
-                    models.CharField(blank=True, max_length=150, verbose_name="last name"),
+                    models.CharField(
+                        blank=True, max_length=150, verbose_name="last name"
+                    ),
                 ),
                 (
                     "email",
-                    models.EmailField(blank=True, max_length=254, verbose_name="email address"),
+                    models.EmailField(
+                        blank=True, max_length=254, verbose_name="email address"
+                    ),
                 ),
                 (
                     "is_staff",
@@ -168,7 +190,8 @@ class Migration(migrations.Migration):
                 (
                     "date_joined",
                     models.DateTimeField(
-                        default=django.utils.timezone.now, verbose_name="date joined"
+                        default=django.utils.timezone.now,
+                        verbose_name="date joined",
                     ),
                 ),
                 ("Bonuses", models.IntegerField(default=0)),
