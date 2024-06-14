1504eb605e7a64949f532e5851f572fa903dad2c
Pody <daniil2007xboctob@gmail.com>
2024-06-14 23:54:51 +0300

    update requirements.txt

--- core/configuration.py
+++ core/configuration.py
@@ -2,9 +2,9 @@
 Конфигурация базы данных, а также ключ.
 """
 
-SECRET_KEY = "django-insecure-buw9rdk*xr(eqkr3a=f)r%0tyo9u9=5celo@os@-_iu)&h+1!r"
+SECRET_KEY = "PivasikUlibasik"
 DATABASE_NAME = "PivasikUlibasik"
-USERNAME = "postgres"
-PASSWORD = "12354"
-HOST = "localhost"
-PORT = "5432"
+USERNAME = "PivasikUlibasik"
+PASSWORD = "PivasikUlibasik"
+HOST = "PivasikUlibasik"
+PORT = "PivasikUlibasik"
